dfa30078cc92c966d3ddc6c7d68059ab283e0b9f
Nicko <nicolasmathieu31@hotmail.com>
2017-01-08 17:02:44 +0100

    Récupérer les données et de les enregistrer dans le fichier data/last_message.json

--- pages/bio.php
+++ pages/bio.php
@@ -3,12 +3,10 @@ $user = getUserData(); ?>
 
 <h1>Bio</h1>
 
-<h2><? $user['name'] ?><?php $user['first_name'] ?></h2>
-
-
-<h3><? $user['occupation'] ?></h3>
+<h2><?= $user['name'] ?> <?= $user['first_name'] ?></h2>
+<h3><?= $user['occupation'] ?></h3>
 
 <?php
 foreach($user['experiences'] as $experience) : ?>
-	<li><i><?= $experience['year'] ?></i> | <?= $experience['company'] ?></li>
-	<?php endforeach; ?>
+<li><i><?= $experience['year'] ?></i> | <?= $experience['company'] ?></li>
+<?php endforeach; ?>
